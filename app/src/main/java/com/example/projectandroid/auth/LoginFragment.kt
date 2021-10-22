9eab45d280afc4e560e800cf5fc1b705a1bace69
Bartosz <bartekj1221@wp.pl>
2021-10-22 19:19:44 +0200

    Added Login and Register screens; Nav graph to navigate between auth screens

---
+++ app/src/main/java/com/example/projectandroid/auth/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.projectandroid.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projectandroid.R
+import com.example.projectandroid.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
