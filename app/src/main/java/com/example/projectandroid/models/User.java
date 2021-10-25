a956ebf6e13441647e3d7c3ecc1425ac5a5f4ea9
Bartosz <bartekj1221@wp.pl>
2021-10-25 17:35:40 +0200

    added user model

---
+++ app/src/main/java/com/example/projectandroid/models/User.java
@@ -0,0 +1,38 @@
+package com.example.projectandroid.models;
+
+import java.util.Objects;
+
+public class User {
+    private final String username;
+    private String avatarUrl;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(String username, String avatarUrl) {
+        this.username = username;
+        this.avatarUrl = avatarUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(avatarUrl, user.avatarUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, avatarUrl);
+    }
+}
