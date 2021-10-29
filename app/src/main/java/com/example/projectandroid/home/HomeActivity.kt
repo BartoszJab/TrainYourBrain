f0ac4d31b296510e4066f904c9e1b44354c2aa45
Bartosz <bartekj1221@wp.pl>
2021-10-29 16:10:59 +0200

    HomeActivity with bottom navigation to move between home fragments

--- app/src/main/java/com/example/projectandroid/home/HomeActivity.kt
+++ app/src/main/java/com/example/projectandroid/home/HomeActivity.kt
@@ -2,11 +2,30 @@ package com.example.projectandroid.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.projectandroid.R
+import com.example.projectandroid.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)
+                as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.playersFragment, R.id.mainFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
