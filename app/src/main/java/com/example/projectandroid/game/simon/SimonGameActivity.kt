a1a270f4a48fde771c27136301bb9c4cf6cb2d68
Bartosz <bartekj1221@wp.pl>
2021-11-02 20:41:00 +0100

    Saving highest round number achieved in Simon game to Firestore

--- app/src/main/java/com/example/projectandroid/game/simon/SimonGameActivity.kt
+++ app/src/main/java/com/example/projectandroid/game/simon/SimonGameActivity.kt
@@ -5,29 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.WindowManager
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.projectandroid.R
 import com.example.projectandroid.databinding.ActivitySimonGameBinding
+import com.example.projectandroid.models.Simon
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 import java.util.*
 import kotlin.concurrent.schedule
 
 class SimonGameActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySimonGameBinding
+    private lateinit var binding: ActivitySimonGameBinding
     private val viewModel: SimonViewModel by viewModels()
-    lateinit var heartsViews: List<ImageView>
+    private lateinit var heartsViews: List<ImageView>
+    private lateinit var myAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySimonGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        myAuth = FirebaseAuth.getInstance()
+        val simonCollectionRef = Firebase.firestore.collection("simon")
 
         val buttons = listOf(
             binding.iv1, binding.iv2, binding.iv3,
@@ -48,7 +59,7 @@ class SimonGameActivity : AppCompatActivity() {
                     } else {
                         buttons[i].setColorFilter(Color.RED)
                     }
-                    delay(500)
+                    delay(300)
                     buttons[i].setColorFilter(Color.GRAY)
                 }
 
@@ -67,9 +78,9 @@ class SimonGameActivity : AppCompatActivity() {
                     WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
                 )
                 for (i in newGeneratedSequence) {
-                    delay(1000)
+                    delay(600)
                     buttons[i].setColorFilter(Color.GREEN)
-                    delay(1000)
+                    delay(600)
                     buttons[i].setColorFilter(Color.GRAY)
                 }
                 window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
@@ -80,6 +91,25 @@ class SimonGameActivity : AppCompatActivity() {
             if (newMistakesNumber > 0) {
                 heartsViews[newMistakesNumber - 1].setImageResource(R.drawable.ic_heart_broken)
                 if (newMistakesNumber == MAX_MISTAKES) {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            val documentSnapshot = simonCollectionRef.document(myAuth.currentUser!!.uid).get().await()
+                            val highestRound = documentSnapshot.toObject<Simon>()?.highestRound
+
+                            // if round number is better than previous one then update the value
+                            val currentRoundNumber = viewModel.roundNumber.value!!
+                            if (currentRoundNumber > highestRound!!) {
+                                simonCollectionRef.document(myAuth.currentUser!!.uid)
+                                    .set(Simon(currentRoundNumber))
+                            }
+
+                            //simonCollectionRef.add(Simon(viewModel.roundNumber.value!!))
+                        } catch (e: Exception) {
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(this@SimonGameActivity, e.message, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
                     showFinalDialog()
                 }
             }
---
+++ app/src/main/java/com/example/projectandroid/models/Simon.java
@@ -0,0 +1,19 @@
+package com.example.projectandroid.models;
+
+public class Simon {
+    public int highestRound;
+
+    public Simon() { }
+
+    public Simon(int highestRound) {
+        this.highestRound = highestRound;
+    }
+
+    public int getHighestRound() {
+        return highestRound;
+    }
+
+    public void setHighestRound(int highestRound) {
+        this.highestRound = highestRound;
+    }
+}
