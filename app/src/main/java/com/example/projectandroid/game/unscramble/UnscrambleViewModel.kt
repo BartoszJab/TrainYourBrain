2dd6d2673e01eaa5b60f826858b8c0b6d6df2136
Bartosz <bartekj1221@wp.pl>
2021-11-03 15:32:31 +0100

    Unscramble game added

--- app/src/main/java/com/example/projectandroid/game/unscramble/UnscrambleViewModel.kt
+++ app/src/main/java/com/example/projectandroid/game/unscramble/UnscrambleViewModel.kt
@@ -1,12 +1,21 @@
 package com.example.projectandroid.game.unscramble
 
+import android.app.Application
 import android.text.Spannable
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.projectandroid.R
+import java.util.*
 
-class UnscrambleViewModel : ViewModel() {
+class UnscrambleViewModel(application: Application) : AndroidViewModel(application) {
+
+    companion object {
+        const val MAX_NUMBER_OF_WORDS = 10
+    }
+
     private val _score = MutableLiveData(0)
     val score: LiveData<Int>
         get() = _score
@@ -22,15 +31,18 @@ class UnscrambleViewModel : ViewModel() {
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
-    private fun getNextWord() {
-        //currentWord = allWordsList.random()
-        val tempWord = currentWord.toCharArray()
-        tempWord.shuffle()
+    init {
+        getNextWord()
+    }
 
-        while (tempWord.joinToString("").equals(currentWord, false)) {
+    private fun getNextWord() {
+        val allWordsList = getApplication<Application>().resources.getStringArray(R.array.words)
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+
+        do {
             tempWord.shuffle()
-            Log.d("getNextWord", "Inside loop")
-        }
+        } while (tempWord.joinToString("").equals(currentWord, false))
 
         if (wordsList.contains(currentWord)) {
             getNextWord()
@@ -40,5 +52,42 @@ class UnscrambleViewModel : ViewModel() {
             _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
         }
+    }
+
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value!! < MAX_NUMBER_OF_WORDS) {
+            getNextWord()
+            return true
+        } else false
+    }
+
+    private fun increaseScore() {
+        // add SCORE_INCREASE to _score.value
+        _score.value = _score.value?.plus(getScoreToAdd())
+    }
+
+    private fun getScoreToAdd(): Int {
+        return when (_currentScrambledWord.value?.length) {
+            in 1..4 -> 5
+            in 5..8 -> 10
+            else -> {
+                15
+            }
+        }
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
     }
 }
---
+++ app/src/main/java/com/example/projectandroid/models/Unscramble.java
@@ -0,0 +1,19 @@
+package com.example.projectandroid.models;
+
+public class Unscramble {
+    public int highestScore;
+
+    public Unscramble() { }
+
+    public Unscramble(int highestScore) {
+        this.highestScore = highestScore;
+    }
+
+    public int getHighestScore() {
+        return highestScore;
+    }
+
+    public void setHighestScore(int highestScore) {
+        this.highestScore = highestScore;
+    }
+}
