3dbc9da0915fb5b3abd2b214c10444e15b981e2c
Bartosz <bartekj1221@wp.pl>
2022-01-16 16:18:33 +0100

    Update

--- app/src/main/java/com/example/projectandroid/game/unscramble/UnscrambleIntroFragment.kt
+++ app/src/main/java/com/example/projectandroid/game/unscramble/UnscrambleIntroFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.projectandroid.R
 import com.example.projectandroid.databinding.FragmentUnscrambleIntroBinding
+import com.example.projectandroid.game.observation.ObservationDifficulty
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class UnscrambleIntroFragment : Fragment() {
 
@@ -27,6 +29,19 @@ class UnscrambleIntroFragment : Fragment() {
 
         binding.btnPlay.setOnClickListener {
             findNavController().navigate(R.id.action_unscrambleIntroFragment_to_unscrambleGameFragment)
+        }
+
+        // TODO: export to string file
+        binding.imgBtnHint.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.observation_difficulty_description_label))
+                .setMessage(
+                    "Słowa o długości znaków\n\n" +
+                            "do 4 -> 5 punktów\n\n" +
+                            "5 do 8 -> 10 punktów\n\n" +
+                            "9 i więcej -> 15 punktów"
+                )
+                .show()
         }
     }
 
